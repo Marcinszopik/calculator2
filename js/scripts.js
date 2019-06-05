497e3eb613b7ed3cdb201dfabce9a7dfb56d9fbd
Joseph Tubridy and Marcin Szopik <marcin@>
2019-06-05 14:57:24 -0700

    change submits to buttons

--- js/scripts.js
+++ js/scripts.js
@@ -21,6 +21,13 @@ var divide = function(number1, number2) {
 // User Interface or front-end logic:
 $().ready(function() {
 
+  /*$("form#math").submit(function(event){
+    event.preventDefault();
+    var number1 = parseInt($("#num1").val());
+    var number2 = parseInt($("#num2").val());
+    var result = subtract(number1, number2);
+    $("#output").text(result);
+  });
 
   $("form#math").submit(function(event){
     event.preventDefault();
@@ -29,14 +36,32 @@ $().ready(function() {
   var result = add(number1, number2);
   $("#output").text(result);
 
+});*/
+
+
+  $("#add").click(function(){
+    var num1 = parseInt($("#num1").val());
+    var num2 = parseInt($("#num2").val());
+    $("#output").text(add(num1, num2));
   });
-  $("form#math").submit(function(event){
-    event.preventDefault();
-    var number1 = parseInt($("#num1").val());
-    var number2 = parseInt($("#num2").val());
-    var result = subtract(number1, number2);
-    $("#output").text(result);
+
+$("#subtract").click(function(){
+     var num1 = parseInt($("#num1").val());
+     var num2 = parseInt($("#num2").val());
+     $("#output").text(subtract(num1, num2));
   });
+
+$("#divide").click(function(){
+     var num1 = parseInt($("#num1").val());
+     var num2 = parseInt($("#num2").val());
+     $("#output").text(divide(num1, num2));
+  });
+
+  $("#multiply").click(function(){
+       var num1 = parseInt($("#num1").val());
+       var num2 = parseInt($("#num2").val());
+       $("#output").text(multiply(num1, num2));
+    });
 
   /*$("form#math").submit(function(event){
     event.preventDefault();
